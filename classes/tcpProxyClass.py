5bc3aa26ebacd9f8dbd5da6f588d2eb7820e8856
Ben Ledingham <benledingham1@gmail.com>
2022-11-27 21:04:54 -0800

    proxy server makes basic request and response

--- classes/tcpProxyClass.py
+++ classes/tcpProxyClass.py
@@ -2,20 +2,34 @@ from socket import *
 
 class ProxyServer:
 
-    def __init__(self, port, server_ip):
+    def __init__(self, port, server_port, server_ip,):
         self.port = port
         self.server_ip = server_ip
+        self.server_port = server_port
 
     def init_socket(self):
-         # Create server socket
+        # Create server socket
         self.socket = socket(AF_INET, SOCK_STREAM)
         #serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         self.socket.bind(('', self.port))
         self.socket.listen(1)
         pass
 
-    def __request_server():
-        pass
+    def __request_server(self, request_content):
+        # Create client socket
+        clientSocket = socket(AF_INET, SOCK_STREAM)
+        clientSocket.connect((self.server_ip, self.server_port))
+        # Send the client request
+        request = request_content
+        clientSocket.send(request.encode())
+
+        # Get HTTP response
+        response = clientSocket.recv(1024)
+        print("From Server:", response.decode())\
+
+        # Close socket
+        clientSocket.close()
+        return response
 
     def run(self):
         print('Listening on port %s ...' % self.port)
@@ -26,31 +40,10 @@ class ProxyServer:
         
             # Get the client request
             request = connectionSocket.recv(1024).decode()
-            print(request)
-
-            # Parse HTTP headers
-            headers = request.split('\n')
-            filename = headers[1].split()[0]
-            print(filename)
-            if headers[0].split()[0] != 'GET' or headers[0].split()[2] != 'HTTP/1.1':
-                response = 'HTTP/1.1 400 BAD REQUEST\n\nBad Request'
-
-            elif headers[0].split()[1] == '/' and filename != '/test.html':
-                response = 'HTTP/1.1 404 NOT FOUND\n\n%s Not Found' % filename
-
-            elif headers[0].split()[1] == '/' and filename == '/test.html':
-                webpage = open('.' + filename)
-                content = webpage.read()
-                if content != prev_content:
-                    prev_content = content
-                    webpage.close()
-                    response = 'HTTP/1.1 200 OK\n\n' + content 
-                else:
-                    webpage.close()
-                    response = 'HTTP/1.1 304 NOT MODIFIED\n\nNot Modified'
+            response = self.__request_server(request_content=request)
 
             # Send HTTP response
-            connectionSocket.sendall(response.encode())
+            connectionSocket.sendall(response)
             # Close socket
             connectionSocket.close()
         pass
